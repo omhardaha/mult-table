a102ad77fc4cf750dfe653553a2fee40e1e0b62f
omhardaha <omprakashhardaha369@gmail.com>
2023-10-19 00:20:48 +0530

    added bootstrap

--- index.js
+++ index.js
@@ -1,34 +1,56 @@
 import fs from 'fs';
 import chalk from 'chalk';
 
-const input = 23;
+const input = 20;
 
 const generateRaw = (num, i, mul) => {
   if (i > num) return '';
-  return `   <td>${i * mul}</td>\n${generateRaw(num, i + 1, mul)}`;
+  return `        <td scope="row">${i * mul}</td>\n${generateRaw(num, i + 1, mul)}`;
 };
 
 const multTable = (n, i) => {
   if (i > 10) return '';
-  return `   <tr>\n${generateRaw(n, 1, i)}</tr>\n${multTable(n, i + 1)}`;
+  return `       <tr class="table-Primary">\n${generateRaw(n, 1, i)}       </tr>\n${multTable(n, i + 1)}`;
 };
-const generateRawAscci = (num, i, mul) => {
+
+const generateRawAscii = (num, i, mul) => {
   if (i > num) return '';
-  return `${i * mul} ${generateRawAscci(num, i + 1, mul)}`;
+  return ` | ${i * mul} ${generateRawAscii(num, i + 1, mul)} |`;
 };
 
 const multTableAscii = (n, i) => {
   if (i > 10) return '';
-  return `${generateRawAscci(n, 1, i)}  \n ${multTableAscii(n, i + 1)}`;
+  return `${generateRawAscii(n, 1, i)}  \n ${multTableAscii(n, i + 1)}`;
 };
+const start = `<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+  </head>
+  <body>`;
+
+const end = `
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>`;
 
 try {
-  fs.writeFileSync('output.html', `<table>\n${multTable(input, 1)}\n</table>`);
+  fs.writeFileSync('output.html', `${start}\n    <table  class="table table-dark table-sm">\n${multTable(input, 1)}    </table>${end}`);
   console.log(chalk.green('output.html created succesfully at ./output.html'));
   fs.writeFileSync('output.txt', ` ${multTableAscii(input, 1)}`);
   console.log(chalk.green('output.txt created succesfully at ./output.txt'));
 } catch (error) {
   console.log(chalk.red('Failed to create files.'));
+  console.log(error);
 }
 // fs.writeFileSync('ascii.txt', `<table>${multTable(input, 1)}</table>`);
 // console.log(multTable(input, 1));
